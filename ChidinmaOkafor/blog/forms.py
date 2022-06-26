52280ec8d8094878f72eab82efb4c7476a9b2d02
Agbani123 <okaforjulia2016@gmail.com>
2022-06-26 21:44:40 +0100

    Changes

---
+++ ChidinmaOkafor/blog/forms.py
@@ -0,0 +1,12 @@
+from socket import fromshare
+from django import forms
+from .models import Post
+
+#create our forms
+class PostForms(forms.ModelForm):
+    #create a meta class What is a meta class?
+    class Meta:
+        model=Post
+        fields=[
+            'title', 'author', 'body', 'status',
+        ]
